4199f4e1795d74446c098136d1a3b3b89942b290
RS4POWER <alexandru.regulas@student.upt.ro>
2023-11-17 16:49:11 +0200

    main

--- Main.cxx
+++ Main.cxx
@@ -1,57 +1,39 @@
 ﻿#include "Car.h"
+#include "ElectricCar.h"
+#include "HybridCar.h"
 #include <iostream>
 
+using namespace Vehicles;
+
 int main() {
-    // Constructor implicit
-    std::cout << "----- Constructor implicit -----" << std::endl;
-    Car car1; // Constructor implicit
-    std::cout << "Car 1 Created" << std::endl;
+    std::cout << "----- Car -----" << std::endl;
+    Vehicles::Car car("Toyota", 2022, 30000.0);
+    car.DisplayInfo();
+    std::cout << std::endl;
 
-    // Constructor personalizat
-    std::cout << "----- Constructor personalizat -----" << std::endl;
-    Car car2("Toyota", 2020, 25000.0); // Constructor personalizat
-    std::cout << "Car 2 Created" << std::endl;
+    std::cout << "----- ElectricCar -----" << std::endl;
+    Vehicles::ElectricCar electricCar("Tesla", 2022, 80000.0, 75.0);
+    electricCar.DisplayInfo();
+    std::cout << "Battery Capacity: " << electricCar.GetBatteryCapacity() << " kWh" << std::endl;
+    std::cout << std::endl;
 
-    // Copy constructor
-    std::cout << "----- Copy constructor -----" << std::endl;
-    Car car3 = car2; // Copy constructor
-    std::cout << "Car 3 (Copy of Car 2) Created" << std::endl;
+    std::cout << "----- HybridCar -----" << std::endl;
+    Vehicles::HybridCar hybridCar("Toyota", 2022, 35000.0, 45.0);
+    hybridCar.DisplayInfo();
+    std::cout << "Fuel Capacity: " << hybridCar.GetFuelCapacity() << " gallons" << std::endl;
 
-    // Modificarea brand-ului pentru car2
-    car2.SetBrand("Honda");
+    std::cout << "----- Copy Assignment Operator -----" << std::endl;
+    Vehicles::Car carCopy;
+    carCopy = car;
+    carCopy.DisplayInfo();
+    std::cout << std::endl;
 
-    // Move constructor
-    std::cout << "----- Move constructor -----" << std::endl;
-    Car car4(std::move(car2)); // Move constructor
-    std::cout << "Car 4 (Moved from Car 2) Created" << std::endl;
-
-    // Verificarea stării obiectelor
-    std::cout << "----- State of Objects -----" << std::endl;
-    if (car1.GetBrand()) {
-        std::cout << "Car 1 Brand: " << car1.GetBrand() << ", Year: " << car1.GetYear() << ", Price: " << car1.GetPrice() << std::endl;
-    }
-    else {
-        std::cout << "Car 1 has no brand." << std::endl;
-    }
-    if (car2.GetBrand()) {
-        std::cout << "Car 2 Brand: " << car2.GetBrand() << ", Year: " << car2.GetYear() << ", Price: " << car2.GetPrice() << std::endl;
-    }
-    else {
-        std::cout << "Car 2 has no brand." << std::endl;
-    }
-    if (car3.GetBrand()) {
-        std::cout << "Car 3 Brand: " << car3.GetBrand() << ", Year: " << car3.GetYear() << ", Price: " << car3.GetPrice() << std::endl;
-    }
-    else {
-        std::cout << "Car 3 has no brand." << std::endl;
-    }
-    if (car4.GetBrand()) {
-        std::cout << "Car 4 Brand: " << car4.GetBrand() << ", Year: " << car4.GetYear() << ", Price: " << car4.GetPrice() << std::endl;
-    }
-    else {
-        std::cout << "Car 4 has no brand." << std::endl;
-    }
+    std::cout << "----- Move Assignment Operator -----" << std::endl;
+    Vehicles::Car carMove;
+    carMove = std::move(car);
+    carMove.DisplayInfo();
+    car.DisplayInfo(); // Verificare că obiectul sursă a fost "mutat"
+    std::cout << std::endl;
 
     return 0;
 }
-
