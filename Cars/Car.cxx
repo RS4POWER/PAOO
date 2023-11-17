32d3eea50974841745a77b66b7f4bece18768293
RS4POWER <alexandru.regulas@student.upt.ro>
2023-11-17 16:26:17 +0200

    Tema2 modificare continut clasa car

--- Cars/Car.cxx
+++ Cars/Car.cxx
@@ -1,88 +1,107 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include "Car.h"
+#include "Car.hpp"
 #include <cstring>
-#include <iostream>
 
-Car::Car() : brand(nullptr), year(0), price(0.0) {
-    std::cout << "Constructor implicit called." << std::endl;
-}
+namespace Vehicles {
+    Car::Car() : brand(nullptr), year(0), price(0.0) {
+        std::cout << "Constructor implicit called." << std::endl;
+    }
 
-Car::Car(const char* brand, int year, double price) : year(year), price(price) {
-    std::cout << "Constructor personalizat called." << std::endl;
-    if(brand)
-    {
+    Car::Car(const char* brand, int year, double price) : year(year), price(price) {
+        std::cout << "Constructor personalizat called." << std::endl;
+        if (brand) {
+            size_t length = strlen(brand);
+            this->brand = new char[length + 1];
+            strcpy(this->brand, brand);
+        }
+        else {
+            this->brand = nullptr;
+        }
+    }
+
+    Car::Car(const Car& other) : year(other.year), price(other.price) {
+        std::cout << "Copy constructor called." << std::endl;
+        CopyData(other);
+    }
+
+    Car::Car(Car&& other) noexcept : brand(other.brand), year(other.year), price(other.price) {
+        std::cout << "Move constructor called." << std::endl;
+        other.brand = nullptr;
+        other.year = 0;
+        other.price = 0.0;
+    }
+
+    Car::~Car() {
+        std::cout << "Destructor called." << std::endl;
+        ReleaseData();
+    }
+
+    Car& Car::operator=(const Car& other) {
+        if (this == &other) {
+            return *this;
+        }
+
+        year = other.year;
+        price = other.price;
+        ReleaseData();
+        CopyData(other);
+
+        return *this;
+    }
+
+    void Car::SetBrand(const char* brand) {
+        if (this->brand) {
+            delete[] this->brand;
+        }
         size_t length = strlen(brand);
-        this->brand = new char [length +1];
-        strncpy(this->brand, brand, length+1);
+        this->brand = new char[length + 1];
+        strcpy(this->brand, brand);
     }
-    else
-    {
-        this->brand = nullptr;
+
+    const char* Car::GetBrand() const {
+        return brand;
     }
-}
 
-Car::Car(const Car& other) : year(other.year), price(other.price) {
-    std::cout << "Copy constructor called." << std::endl;
-    CopyData(other);
-}
-
-Car::Car(Car&& other) noexcept : brand(other.brand), year(other.year), price(other.price) {
-    std::cout << "Move constructor called." << std::endl;
-    other.brand = nullptr;
-    other.year = 0;
-    other.price = 0.0;
-}
-
-Car::~Car() {
-    std::cout << "Destructor called." << std::endl;
-    ReleaseData();
-}
-
-void Car::SetBrand(const char* brand) {
-    if (this->brand) {
-        delete[] this->brand;
+    void Car::SetYear(int year) {
+        this->year = year;
     }
-    size_t length = strlen(brand);
-    this->brand = new char[length + 1];
-    strncpy(this->brand, brand, length +1);
-}
 
-const char* Car::GetBrand() const {
-    return brand;
-}
-
-void Car::SetYear(int year) {
-    this->year = year;
-}
-
-int Car::GetYear() const {
-    return year;
-}
-
-void Car::SetPrice(double price) {
-    this->price = price;
-}
-
-double Car::GetPrice() const {
-    return price;
-}
-
-void Car::CopyData(const Car& other) {
-    if(other.brand)
-    {
-        size_t length = strlen(other.brand);
-        this->brand = new char[length + 1 ];
-        strncpy(this->brand, other.brand, length + 1);
+    int Car::GetYear() const {
+        return year;
     }
-    else
-    {
-        this->brand = nullptr;
-    }
-}
 
-void Car::ReleaseData() {
-    if (brand) {
-        delete[] brand;
-        brand = nullptr;
+    void Car::SetPrice(double price) {
+        this->price = price;
     }
-}
+
+    double Car::GetPrice() const {
+        return price;
+    }
+
+    void Car::CopyData(const Car& other) {
+        if (other.brand) {
+            size_t length = strlen(other.brand);
+            this->brand = new char[length + 1];
+            strcpy(this->brand, other.brand);
+        }
+        else {
+            this->brand = nullptr;
+        }
+    }
+
+    void Car::DisplayInfo() const {
+        std::cout << "Brand: " << brand << ", Year: " << year << ", Price: " << price << std::endl;
+    }
+
+    void Car::ReleaseData() {
+        if (brand) {
+            delete[] brand;
+            brand = nullptr;
+        }
+    }
+
+    std::ostream& operator<<(std::ostream& os, const Car& car) {
+        os << "Brand: " << car.brand << ", Year: " << car.year << ", Price: " << car.price;
+        return os;
+    }
+} // namespace Vehicles
--- Cars/Car.h
+++ Cars/Car.h
@@ -1,24 +1,37 @@
+#ifndef CAR_H
+#define CAR_H
+
+#include <iostream>
 #pragma once
-class Car {
-public:
-    Car();
-    Car(const char* brand, int year, double price); 
-    Car(const Car& other); 
-    Car(Car&& other) noexcept; 
-    ~Car(); 
+namespace Vehicles {
+    class Car {
+    public:
+        Car();
+        Car(const char* brand, int year, double price);
+        Car(const Car& other);
+        Car(Car&& other) noexcept;
+        virtual ~Car(); // Trebuie să fie virtuală pentru a asigura apelul destructorului derivatelor.
 
-    void SetBrand(const char* brand);
-    const char* GetBrand() const;
-    void SetYear(int year);
-    int GetYear() const;
-    void SetPrice(double price);
-    double GetPrice() const;
+        Car& operator=(const Car& other);
 
-private:
-    char* brand;
-    int year;
-    double price;
+        void SetBrand(const char* brand);
+        const char* GetBrand() const;
+        void SetYear(int year);
+        int GetYear() const;
+        void SetPrice(double price);
+        double GetPrice() const;
 
-    void CopyData(const Car& other);
-    void ReleaseData();
-};
+        virtual void DisplayInfo() const; // Adaugare functie virtuala DisplayInfo
+        friend std::ostream& operator<<(std::ostream& os, const Car& car);
+
+    private:
+        char* brand;
+        int year;
+        double price;
+
+        void CopyData(const Car& other);
+        void ReleaseData();
+    };
+} // namespace Vehicles
+
+#endif // CAR_HP
