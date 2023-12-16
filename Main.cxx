3a6ad34115ed27461a6868e2eb35eee63a24aaca
RS4POWER <alexandru.regulas@student.upt.ro>
2023-12-16 15:01:18 +0200

    main for testing threads

--- Main.cxx
+++ Main.cxx
@@ -1,65 +1,57 @@
-﻿#include "Car.h"
-#include "ElectricCar.h"
-#include "HybridCar.h"
-#include <iostream>
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <memory>
+#include <mutex>
+
+#include "Car.h"
+#include "ParkingLot.h"
 
 int main() {
-    // Exemplu cu mașină electrică în namespace-ul Vehicles
-    std::cout << "======= Electric Car Example in Vehicles Namespace =======" << std::endl;
+    // Creăm o parcare cu o capacitate de 5 locuri
+    ParkingLot parkingLot(5);
 
-    // Creare obiect ElectricCar utilizând constructorul
-    std::cout << "Creating ElectricCar object:" << std::endl;
-    Vehicles::ElectricCar electricCar("Tesla", 2022, 80000.0, 75.0);
+    // Creăm două mașini folosind smart pointers
+    std::shared_ptr<Car> car1 = std::make_shared<Car>("AR34AEX", true);  // Această mașină necesită un loc special pentru handicap
+    std::shared_ptr<Car> car2 = std::make_shared<Car>("AR21AIR");
+    std::shared_ptr<Car> car3 = std::make_shared<Car>("AR04INA", true);  // Această mașină necesită un loc special pentru handicap
+    std::shared_ptr<Car> car4 = std::make_shared<Car>("AR17ALX");
 
-    // Afisare informatii inainte de operatii de copiere/mutare
-    std::cout << "Displaying Electric Car Information Before Operations:" << std::endl;
-    electricCar.DisplayInfo();
+    // Adăugăm mașinile în parcare
+    parkingLot.addCar(car1,true);
+    parkingLot.addCar(car2);
+    parkingLot.addCar(car3,true);
+    parkingLot.addCar(car4);
 
-    // Copiere ElectricCar folosind constructorul de copiere personalizat
-    std::cout << "Creating a copy of ElectricCar using Copy Constructor:" << std::endl;
-    Vehicles::ElectricCar electricCarCopy = electricCar;
 
-    // Afisare informatii dupa operatia de copiere
-    std::cout << "Displaying Electric Car Information After Copy Operation:" << std::endl;
-    electricCarCopy.DisplayInfo();
+    // Afisam starea initiala a parcarilor
+    parkingLot.printParkingStatus();
 
-    // Mutare ElectricCar folosind operatorul de mutare personalizat
-    std::cout << "Moving ElectricCar using Move Assignment Operator:" << std::endl;
-    Vehicles::ElectricCar electricCarMoved = std::move(electricCar);
+    // Definim o functie care simuleaza intrarea si iesirea periodica a unei masini din parcare
+    auto simulateCarEntryExit = [&parkingLot](std::shared_ptr<Car> car, int entryDelay, int exitDelay) {
+        while (true) {
+            std::this_thread::sleep_for(std::chrono::seconds(entryDelay));
+            parkingLot.enter(car);
+            parkingLot.printParkingStatus();
+            std::this_thread::sleep_for(std::chrono::seconds(exitDelay));
+            parkingLot.exit(car);
+            parkingLot.printParkingStatus();
+        }
+        };
 
-    // Afisare informatii dupa operatia de mutare
-    std::cout << "Displaying Electric Car Information After Move Operation:" << std::endl;
-    electricCarMoved.DisplayInfo();
+    // Pornim fire de execuție pentru cele patru mașini
+    std::thread threadCar1(simulateCarEntryExit, car1, 3, 5);
+    std::thread threadCar2(simulateCarEntryExit, car2, 4, 6);
+    std::thread threadCar3(simulateCarEntryExit, car3, 5, 7);
+    std::thread threadCar4(simulateCarEntryExit, car4, 6, 8);
 
-    std::cout << std::endl;
-
-    // Exemplu cu mașină hibridă în namespace-ul Vehicles
-    std::cout << "======= Hybrid Car Example in Vehicles Namespace =======" << std::endl;
-
-    // Creare obiect HybridCar utilizând constructorul
-    std::cout << "Creating HybridCar object:" << std::endl;
-    Vehicles::HybridCar hybridCar("Toyota", 2022, 35000.0, 45.0);
-
-    // Afisare informatii inainte de operatii de copiere/mutare
-    std::cout << "Displaying Hybrid Car Information Before Operations:" << std::endl;
-    hybridCar.DisplayInfo();
-
-    // Copiere HybridCar folosind constructorul de copiere personalizat
-    std::cout << "Creating a copy of HybridCar using Copy Constructor:" << std::endl;
-    Vehicles::HybridCar hybridCarCopy = hybridCar;
-
-    // Afisare informatii dupa operatia de copiere
-    std::cout << "Displaying Hybrid Car Information After Copy Operation:" << std::endl;
-    hybridCarCopy.DisplayInfo();
-
-    // Mutare HybridCar folosind operatorul de mutare personalizat
-    std::cout << "Moving HybridCar using Move Assignment Operator:" << std::endl;
-    Vehicles::HybridCar hybridCarMoved = std::move(hybridCar);
-
-    // Afisare informatii dupa operatia de mutare
-    std::cout << "Displaying Hybrid Car Information After Move Operation:" << std::endl;
-    hybridCarMoved.DisplayInfo();
+    // Așteptăm până când firele de execuție se termină
+    threadCar1.join();
+    threadCar2.join();
+    threadCar3.join();
+    threadCar4.join();
 
     return 0;
 }
 
+//good code
