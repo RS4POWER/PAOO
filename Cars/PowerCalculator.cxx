3576d5cd13c1299e026712632a6c45c1bb4a5b55
RS4POWER <alexandru.regulas@student.upt.ro>
2023-12-16 14:54:29 +0200

    Ultima Tema

--- Cars/PowerCalculator.cxx
+++
@@ -1,8 +0,0 @@
-﻿// PowerCalculator.cxx
-#include "PowerCalculator.h"
-
-namespace Utilities {
-    double PowerCalculator::calculatePower() const {
-        return 0.0; // Implementarea implicită poate fi suprascrisă în clasele derivate
-    }
-} // namespace Utilities
